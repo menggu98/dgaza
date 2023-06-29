f11385f6d6d96d51bcad2dfa0c534bba350e1370
menggu98 <myengkyun@naver.com>
2023-06-29 16:44:23 +0900

    finish

---
+++ 절상절하사사오입/Form1.Designer.vb
@@ -0,0 +1,163 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form은 Dispose를 재정의하여 구성 요소 목록을 정리합니다.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows Form 디자이너에 필요합니다.
+    Private components As System.ComponentModel.IContainer
+
+    '참고: 다음 프로시저는 Windows Form 디자이너에 필요합니다.
+    '수정하려면 Windows Form 디자이너를 사용하십시오.  
+    '코드 편집기에서는 수정하지 마세요.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.input = New System.Windows.Forms.TextBox()
+        Me.output = New System.Windows.Forms.TextBox()
+        Me.round = New System.Windows.Forms.Button()
+        Me.reset = New System.Windows.Forms.Button()
+        Me.choose = New System.Windows.Forms.ComboBox()
+        Me.value = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'input
+        '
+        Me.input.Anchor = System.Windows.Forms.AnchorStyles.Left
+        Me.input.Font = New System.Drawing.Font("굴림", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.input.Location = New System.Drawing.Point(62, 147)
+        Me.input.Multiline = True
+        Me.input.Name = "input"
+        Me.input.Size = New System.Drawing.Size(159, 74)
+        Me.input.TabIndex = 0
+        '
+        'output
+        '
+        Me.output.Anchor = System.Windows.Forms.AnchorStyles.Left
+        Me.output.Font = New System.Drawing.Font("굴림", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.output.Location = New System.Drawing.Point(519, 147)
+        Me.output.Multiline = True
+        Me.output.Name = "output"
+        Me.output.Size = New System.Drawing.Size(159, 74)
+        Me.output.TabIndex = 1
+        '
+        'round
+        '
+        Me.round.Font = New System.Drawing.Font("돋움", 21.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.round.Location = New System.Drawing.Point(62, 248)
+        Me.round.Name = "round"
+        Me.round.Size = New System.Drawing.Size(324, 54)
+        Me.round.TabIndex = 2
+        Me.round.Text = "계산"
+        Me.round.UseVisualStyleBackColor = True
+        '
+        'reset
+        '
+        Me.reset.Font = New System.Drawing.Font("돋움", 21.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.reset.Location = New System.Drawing.Point(392, 248)
+        Me.reset.Name = "reset"
+        Me.reset.Size = New System.Drawing.Size(286, 54)
+        Me.reset.TabIndex = 3
+        Me.reset.Text = "지우기"
+        Me.reset.UseVisualStyleBackColor = True
+        '
+        'choose
+        '
+        Me.choose.FormattingEnabled = True
+        Me.choose.Location = New System.Drawing.Point(392, 147)
+        Me.choose.Name = "choose"
+        Me.choose.Size = New System.Drawing.Size(121, 20)
+        Me.choose.TabIndex = 4
+        '
+        'value
+        '
+        Me.value.Anchor = System.Windows.Forms.AnchorStyles.Left
+        Me.value.Font = New System.Drawing.Font("굴림", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.value.Location = New System.Drawing.Point(227, 147)
+        Me.value.Multiline = True
+        Me.value.Name = "value"
+        Me.value.Size = New System.Drawing.Size(159, 74)
+        Me.value.TabIndex = 5
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(116, 125)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(57, 12)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "값을 입력"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(242, 125)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(133, 12)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "소수점을 처리할 자릿수"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(434, 125)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(29, 12)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "옵션"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(586, 125)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(41, 12)
+        Me.Label4.TabIndex = 9
+        Me.Label4.Text = "결과값"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.value)
+        Me.Controls.Add(Me.choose)
+        Me.Controls.Add(Me.reset)
+        Me.Controls.Add(Me.round)
+        Me.Controls.Add(Me.output)
+        Me.Controls.Add(Me.input)
+        Me.Name = "Form1"
+        Me.Text = "사사오입,절상,절사"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents input As TextBox
+    Friend WithEvents output As TextBox
+    Friend WithEvents round As Button
+    Friend WithEvents reset As Button
+    Friend WithEvents choose As ComboBox
+    Friend WithEvents value As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+End Class
---
+++ 절상절하사사오입/Form1.vb
@@ -0,0 +1,59 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        choose.Items.Add("사사오입")
+        choose.Items.Add("절상")
+        choose.Items.Add("버림")
+    End Sub
+
+    Private Sub round_Click(sender As Object, e As EventArgs) Handles round.Click
+        Dim tmp1 As Double
+        Dim opt As Char
+        Dim cnt As Integer
+        tmp1 = Val(input.Text)
+        opt = choose.Text
+        cnt = value.Text
+        output.Text = cjround(tmp1, opt, cnt)
+    End Sub
+
+    Private Sub reset_Click(sender As Object, e As EventArgs) Handles reset.Click
+        input.Text = ""
+        value.Text = ""
+        output.Text = ""
+    End Sub
+
+    Private Function cjround(pValue As Double, pOpt As Char, pDigit As Integer) As Single
+
+        Dim i As Integer
+        Dim t_s As String, t_u As Single
+
+        t_u = 10 ^ (pDigit * -1) ' pDigit은 처리할 자리 수
+
+        Select Case pOpt ' pOpt는 절상, 절사, 사사오입 중 옵션 선택
+            Case "사사오입"
+                t_u = Int((pValue + (t_u / 2)) / t_u) * t_u ' 사사오입
+            Case "절상"
+                t_u = Int((pValue + (t_u - 10 ^ (-1 * (15 - Len(Format(Int(pValue))))))) / t_u) * t_u
+                ' 절상
+            Case "버림"
+                t_u = Int(pValue) ' 절사   Int(pValue / t_u) * t_u
+            Case Else
+                t_u = pValue
+        End Select
+
+        If pDigit > 0 Then ' 처리할 값이 0보다 크다면
+            t_s = "0."
+            For i = 1 To pDigit
+                t_s = t_s + "0"
+
+            Next i
+            cjround = Format(t_u, t_s)
+        Else
+            cjround = t_u
+        End If
+        cjround = t_u
+    End Function
+
+    Private Sub choose_SelectedIndexChanged(sender As Object, e As EventArgs) Handles choose.SelectedIndexChanged
+
+    End Sub
+End Class
